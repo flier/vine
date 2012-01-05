399d1a886a71e57365af1e58bbd040abb9309368
U-Flier-Laptop\Flier Lu <Flier Lu@Flier-Laptop.(none)>
2012-01-05 20:48:55 +0800

    add OOP supports

--- src/api/list.js
+++ src/api/list.js
@@ -1,36 +1,88 @@
-define(["require", "exports"], function (require, exports) {
+define(["require", "exports", "utils/oop"], function (require, exports) {
 
 var List = function (array) {
-    this.array = array;
-};
+    array = array || [];
 
-List.fn = List.prototype = {
-
-    length: function () {
-        return this.array.length;
-    },
-
-    lpush: function () {
-
-    },
-
-    rpush: function () {
-
-    },
-
-    lpop: function () {
-
-    },
-
-    rpop: function () {
-
+    for (var i in array) {
+        this.push(array[i]);
     }
 
+    this.lpush = this.unshift;
+    this.rpush = this.push;
+    this.lpop = this.shift;
+    this.rpop = this.pop;
+    this.range = this.slice;
 };
+
+List.inherit(Array);
+List.extend({
+    equals: function (list) {
+        if (this.length != list.length) return false;
+
+        for (var i in this) {
+            if (this[i] != list[i]) return false;
+        }
+
+        return true;
+    },
+    remove: function (count, value) {
+        var found = 0;
+
+        if (count > 0) {
+            var i = 0;
+
+            while (count > found && i < this.array.length) {
+                if (this.array[i] == value) {
+                    found++;
+                    this.array.splice(i);
+                } else {
+                    i++;
+                }
+            }
+        } else if (count < 0) {
+            for (var i = this.array.length-1; count <= -found && i >= 0; i--) {
+                if (this.array[i] == value) {
+                    found++;
+                    this.array.splice(i);
+                }
+            }
+        } else {
+            for (var i=this.array.length-1; i>= 0; i--) {
+                if (this.array[i] == value) {
+                    found++;
+                    this.array.splice(i);
+                }
+            }
+        }
+
+        return found;
+    }
+});
 
 exports.List = List;
 exports.tests = function () {
     module("List API");
+
+    test("basic list operation", function () {
+        var l = new List();
+
+        equal(l.length, 0);
+
+        equal(l.rpush('hello'), 1);
+        equal(l.rpush('world'), 2);
+
+        equal(l.length, 2);
+
+        equal(l.lpop(), 'hello');
+
+        equal(l.length, 1);
+
+        equal(l.rpop(), 'world');
+
+        equal(l.lpush(3, 2, 1), 3);
+
+        equal(new List([3, 2, 1]).equals(l.range(-100, 100)));
+    });
 };
 
 });
---
+++ src/utils/oop.js
@@ -0,0 +1,58 @@
+define(["require", "exports"], function (require, exports) {
+
+Function.prototype.inherit = function (parent) {
+    if (parent.constructor == Function) {
+        this.prototype = new parent();
+        this.prototype.constructor = this;
+        this.prototype.parent = parent.prototype;
+    } else {
+        this.prototype = parent;
+        this.prototype.constructor = this;
+        this.prototype.parent = parent;
+    }
+
+    return this;
+};
+
+Function.prototype.extend = function (props) {
+    for ( var prop in props ) {
+        if ( props[prop] === undefined ) {
+            delete this.prototype[prop];
+
+            // Avoid "Member not found" error in IE8 caused by setting window.constructor
+        } else if ( prop !== "constructor" || this !== window ) {
+            this.prototype[prop] = props[prop];
+        }
+    }
+
+    return this;
+};
+
+exports.tests = function () {
+    module("OOP utils");
+
+    test("inherit and extend", function () {
+        var Animal = function () {
+            this.name = function () { return this.type; };
+        };
+
+        var Cat = function () {
+            this.type = "cat";
+        };
+
+        Cat.inherit(Animal).extend({
+            hello: function () {
+                return "hello from " + this.name();
+            }
+        })
+
+        var c = new Cat();
+
+        equal(c.parent, Animal.prototype);
+
+        equal(c.name(), "cat");
+        equal(c.hello(), "hello from cat");
+    });
+};
+
+});
