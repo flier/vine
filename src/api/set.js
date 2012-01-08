af8ce622cc12472344d43c8c0885c88dfa5a24e4
U-Flier-Laptop\Flier Lu <Flier Lu@Flier-Laptop.(none)>
2012-01-08 15:35:24 +0800

    add basic set operation with test cases

---
+++ src/api/set.js
@@ -0,0 +1,84 @@
+define(["require", "exports", "api/list", "utils/oop"], function (require, exports) {
+
+var Set = function (items) {
+    this.items = items || [];
+};
+
+Set.extend({
+    length: function() {
+        return this.items.length;
+    },
+    members: function() {
+        return this.items;
+    },
+    add: function() {
+        var added = 0;
+
+        for (var i=0; i<arguments.length; i++) {
+            if (this.items.find(arguments[i]) < 0) {
+                this.items.push(arguments[i]);
+                added++;
+            }
+        }
+
+        return added;
+    },
+    remove: function() {
+        var removed = 0;
+
+        for (var i=0; i<arguments.length; i++) {
+            var idx = this.items.find(arguments[i]);
+
+            if (idx >= 0) {
+                this.items.splice(idx, 1);
+
+                removed++;
+            }
+        }
+
+        return removed;
+    },
+    contains: function() {
+        for (var i=0; i<arguments.length; i++) {
+            if (this.items.find(arguments[i]) < 0) {
+                return false;
+            }
+        }
+        return true;
+    },
+    pop: function() {
+        var idx = Math.floor(this.items.length * Math.random());
+
+        return this.items.splice(idx, 1)[0];
+    },
+    random: function() {
+        var idx = Math.floor(this.items.length * Math.random());
+
+        return this.items[idx];
+    }
+});
+
+exports.tests = function () {
+    module("Set API");
+
+    test("basic set operation", function () {
+        var s = new Set();
+
+        equals(s.add(1, 2, 3, 3), 3);
+        equals(s.length(), 3);
+        ok(s.members().equals([1, 2, 3]));
+        ok(s.contains(3));
+        ok(!s.contains(4));
+
+        equals(s.add(4, 5), 2);
+        equals(s.remove(4, 5), 2);
+        equals(s.length(), 3);
+        equals(s.remove(4, 5), 0);
+
+        ok(s.contains(s.random()));
+        ok(!s.contains(s.pop()));
+        equals(s.length(), 2);
+    });
+};
+
+});
