27fbad0a5f15f143d167436658b3e36939d4e223
Flier Lu <flier.lu@gmail.com>
2012-01-13 13:20:13 +0800

    support to write/read the IEEE754 double

--- src/utils/blob.js
+++ src/utils/blob.js
@@ -1,4 +1,4 @@
-define("utils/blob", ["require", "exports", "utils/oop"], function (require, exports) {
+define("utils/blob", ["require", "exports", "utils/oop", "api/list"], function (require, exports) {
 
 var Binary = function (buf, off, len) {
     this.buffer = this.getByteView(buf, off, len);
@@ -124,6 +124,121 @@ Binary.extend({
 
             return num;
         }
+    },
+    writeDouble: function (/* num, num, ... */) {
+        for (var i=0; i<arguments.length; i++) {
+            var arg = arguments[i];
+            var num = arg instanceof Number ? arg : parseFloat(arg);
+
+            this.writeIEEE754(num, 'little', 52, 8);
+
+            this.offset += 8;
+        }
+
+        return 8 * arguments.length;
+    },
+    readDouble: function (count) {
+        if (count) {
+            var nums = new Array(count);
+
+            for (var i=0; i<count; i++) {
+                nums[i] = this.readDouble();
+            }
+
+            return nums;
+        } else {
+            var num = this.readIEEE754('little', 52, 8);
+
+            this.offset += 8;
+
+            return num;
+        }
+    },
+    writeIEEE754: function(value, endian, mLen, nBytes) {
+        var e, m, c,
+          bBE = (endian === 'big'),
+          eLen = nBytes * 8 - mLen - 1,
+          eMax = (1 << eLen) - 1,
+          eBias = eMax >> 1,
+          rt = (mLen === 23 ? Math.pow(2, -24) - Math.pow(2, -77) : 0),
+          i = bBE ? (nBytes-1) : 0,
+          d = bBE ? -1 : 1,
+          s = value < 0 || (value === 0 && 1 / value < 0) ? 1 : 0;
+
+        value = Math.abs(value);
+
+        if (isNaN(value) || value === Infinity) {
+            m = isNaN(value) ? 1 : 0;
+            e = eMax;
+        } else {
+            e = Math.floor(Math.log(value) / Math.LN2);
+            if (value * (c = Math.pow(2, -e)) < 1) {
+                e--;
+                c *= 2;
+            }
+            if (e+eBias >= 1) {
+                value += rt / c;
+            } else {
+                value += rt * Math.pow(2, 1 - eBias);
+            }
+            if (value * c >= 2) {
+                e++;
+                c /= 2;
+            }
+
+            if (e + eBias >= eMax) {
+                m = 0;
+                e = eMax;
+            } else if (e + eBias >= 1) {
+                m = (value * c - 1) * Math.pow(2, mLen);
+                e = e + eBias;
+            } else {
+                m = value * Math.pow(2, eBias - 1) * Math.pow(2, mLen);
+                e = 0;
+            }
+        }
+
+        for (; mLen >= 8; this.put(i, m & 0xff), i += d, m /= 256, mLen -= 8);
+
+        e = (e << mLen) | m;
+        eLen += mLen;
+
+        for (; eLen > 0; this.put(i, e & 0xff), i += d, e /= 256, eLen -= 8);
+
+        this.put(i - d, this.get(i - d) | s * 128);
+    },
+    readIEEE754: function(endian, mLen, nBytes) {
+      var e, m,
+          bBE = (endian === 'big'),
+          eLen = nBytes * 8 - mLen - 1,
+          eMax = (1 << eLen) - 1,
+          eBias = eMax >> 1,
+          nBits = -7,
+          i = bBE ? 0 : (nBytes - 1),
+          d = bBE ? 1 : -1,
+          s = this.get(i);
+
+      i += d;
+
+      e = s & ((1 << (-nBits)) - 1);
+      s >>= (-nBits);
+      nBits += eLen;
+      for (; nBits > 0; e = e * 256 + this.get(i), i += d, nBits -= 8);
+
+      m = e & ((1 << (-nBits)) - 1);
+      e >>= (-nBits);
+      nBits += mLen;
+      for (; nBits > 0; m = m * 256 + this.get(i), i += d, nBits -= 8);
+
+      if (e === 0) {
+        e = 1 - eBias;
+      } else if (e === eMax) {
+        return m ? NaN : ((s ? -1 : 1) * Infinity);
+      } else {
+        m = m + Math.pow(2, mLen);
+        e = e - eBias;
+      }
+      return (s ? -1 : 1) * m * Math.pow(2, e - mLen);
     }
 });
 
@@ -226,16 +341,27 @@ exports.tests = function () {
         equals(bin.readUtf8String(), "测试", "readUtf8String till NULL");
         equals(bin.offset, 6);
 
-        equals(bin.slice(3, 6).readUtf8String(), "试");
+        equals(bin.slice(3, 6).readUtf8String(), "试", "slice");
 
         bin.reset();
-        equals(bin.writeInt(123456789, '2'), 8);
+        equals(bin.writeInt(123456789, '2'), 8, "writeInt");
         equals(bin.offset, 8);
 
         bin.reset();
-        equals(bin.readInt(), 123456789);
+        equals(bin.readInt(), 123456789, "readInt");
         equals(bin.offset, 4);
-        equals(bin.readInt(), 2);
+        equals(bin.readInt(), 2, "readInt");
+        equals(bin.offset, 8);
+
+        bin.reset();
+        ok(bin.readInt(2).equals([123456789, 2]), "readInt(count)");
+
+        bin.reset();
+        equals(bin.writeDouble(3.1415926), 8, "writeDouble");
+        equals(bin.offset, 8);
+
+        bin.reset();
+        equals(bin.readDouble(), 3.1415926, "readDouble");
         equals(bin.offset, 8);
     });
 
